3b5cfdd8fe7ae7880cdd4980cdb176787af97f5b
Eric Sarbacker <84537156+Sarbsey@users.noreply.github.com>
2021-05-27 10:27:56 -0400

    Update nuclearity-zoo.py

--- nuclearity-zoo.py
+++ nuclearity-zoo.py
@@ -1,38 +1,34 @@
+import pickle
+import numpy as np
+import pandas as pd
+import plotly.express as px
+import dash
+import dash_table
+from dash_table.Format import Format, Group
 import dash_core_components as dcc
+import dash_html_components as html
+import re
 
-app.layout = html.Div(children=[
-    html.H1('My Dash App'),
-    html.Div(
-        [
-            html.Label('From 2007 to 2017', id='time-range-label'),
-            dcc.RangeSlider(
-                id='year_slider',
-                min=1991,
-                max=2017,
-                value=[2007, 2017]
-            ),
-        ],
-        style={'margin-top': '20'}
-    ), 
-    html.Hr(),
-    dcc.Graph(id='my-graph')
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+app.layout = html.Div([
+    dcc.RangeSlider(
+        id='my-range-slider',
+        min=0,
+        max=20,
+        step=0.5,
+        value=[5, 15]
+    ),
+    html.Div(id='output-container-range-slider')
 ])
 
 
 @app.callback(
-    output=Output('my-graph', 'figure'),
-    inputs=[Input('year_slider', 'value')]
-    )
-def _update_graph(year_range):
-    date_start = '{}-01-01'.format(year_range[0])
-    date_end = '{}-12-31'.format(year_range[1])
-    
-    @app.callback(
-    output=Output('time-range-label', 'children'),
-    inputs=[Input('year_slider', 'value')]
-    )
-def _update_time_range_label(year_range):
-    return 'From {} to {}'.format(year_range[0], year_range[1])
+    dash.dependencies.Output('output-container-range-slider', 'children'),
+    [dash.dependencies.Input('my-range-slider', 'value')])
+def update_output(value):
+    return 'You have selected "{}"'.format(value)
 
 
 if __name__ == '__main__':
