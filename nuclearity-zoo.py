f7799364666ca4dff9b31f318385863d0e0e24ab
Eric Sarbacker <84537156+Sarbsey@users.noreply.github.com>
2021-05-27 10:13:21 -0400

    Update nuclearity-zoo.py

--- nuclearity-zoo.py
+++ nuclearity-zoo.py
@@ -1,30 +1,19 @@
-import dash
-import dash_html_components as html
 import dash_core_components as dcc
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-app.layout = html.Div([
-    html.H1(children='Nuclearity Zoo'),
-    
-    dcc.RangeSlider(
-        id='form_energy',
-        min=0,
-        max=20,
-        step=0.5,
-        value=[5, 15]
-    ),
-    html.Div(id='fe_selection')
+app.layout = html.Div(children=[
+    html.H1('My Dash App'),
+    html.Div(
+        [
+            html.Label('From 2007 to 2017', id='time-range-label'),
+            dcc.RangeSlider(
+                id='year_slider',
+                min=1991,
+                max=2017,
+                value=[2007, 2017]
+            ),
+        ],
+        style={'margin-top': '20'}
+    ), 
+    html.Hr(),
+    dcc.Graph(id='my-graph')
 ])
-
-
-@app.callback(
-    dash.dependencies.Output('fe_selection', 'children'),
-    [dash.dependencies.Input('form_energy', 'value')])
-def update_output(value):
-    return 'You have selected "{}"'.format(value)
-
-
-if __name__ == '__main__':
-    app.run_server(debug=True)
