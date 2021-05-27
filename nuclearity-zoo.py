c6c4ddf4ac333331fe07456e3798713de819daad
Eric Sarbacker <84537156+Sarbsey@users.noreply.github.com>
2021-05-27 10:14:24 -0400

    Update nuclearity-zoo.py

--- nuclearity-zoo.py
+++ nuclearity-zoo.py
@@ -17,3 +17,19 @@ app.layout = html.Div(children=[
     html.Hr(),
     dcc.Graph(id='my-graph')
 ])
+
+
+@app.callback(
+    output=Output('my-graph', 'figure'),
+    inputs=[Input('year_slider', 'value')]
+    )
+def _update_graph(year_range):
+    date_start = '{}-01-01'.format(year_range[0])
+    date_end = '{}-12-31'.format(year_range[1])
+    
+    @app.callback(
+    output=Output('time-range-label', 'children'),
+    inputs=[Input('year_slider', 'value')]
+    )
+def _update_time_range_label(year_range):
+    return 'From {} to {}'.format(year_range[0], year_range[1])
